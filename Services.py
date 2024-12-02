cad58bb1a5c6db5584afd5df33bbea76068965e0
Richard Cunningham <richard.cunningham@diamond.ac.uk>
2024-12-02 14:58:37 +0000

    Sucessfully stored received numbers

--- Services.py
+++ Services.py
@@ -1,17 +1,25 @@
 import time
 import stomp
 import random as rand
+import os
+os.system('clear')
+
 class MyListener(stomp.ConnectionListener):
-    def on_message(self, headers, message):
-        print('MyListener:\nreceived a message "{}"\n'.format(message))
-        global read_messages
-        read_messages.append({'id': headers['message-id'], 'subscription':headers['subscription']})
+    def on_error(self, frame):
+        print('received an error "%s"' % frame.body)
+
+    def on_message(self, frame):
+        print('received a message "%s"' % frame.body)
+        read_messages.append('%s' % frame.body)
 
 read_messages = []
-hosts = [('localhost', 61613)]
-
-conn = stomp.Connection(host_and_ports=hosts)
-conn.set_listener('my_listener', stomp.PrintingListener())
+conn = stomp.Connection()
+conn.set_listener('', MyListener())
 conn.connect(wait=True)
-conn.subscribe(destination='test.req', id=1, ack='client-individual')
-conn.send(destination='test.req', body=str(rand.randint(0,2**16)))
+conn.subscribe(destination='/queue/test', id=1, ack='auto')
+conn.send(body=f'{rand.randint(0,2**16)}', destination='/queue/test')
+
+
+time.sleep(2)
+conn.disconnect()
+print(read_messages)
